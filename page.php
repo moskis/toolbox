d2691c84506d0164e6eecccf9c879371190255fc
themeshaper <themeshaper@d957f892-c61d-0410-b221-f235e6eecf30>
2011-02-17 19:18:51 +0000

    Toolbox: removing loop.php and using content.php to for markup in <article> tags on author, search, index, tag, page, category, single, and archive templates
    git-svn-id: https://wpcom-themes.svn.automattic.com/toolbox@5697 d957f892-c61d-0410-b221-f235e6eecf30

--- page.php
+++ page.php
@@ -11,17 +11,7 @@ get_header(); ?>
 
 				<?php the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'toolbox' ), 'after' => '</div>' ) ); ?>
-						<?php edit_post_link( __( 'Edit', 'toolbox' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-<?php the_ID(); ?> -->
+				<?php get_template_part( 'content', 'page' ); ?>
 
 				<?php comments_template( '', true ); ?>
 
