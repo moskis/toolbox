1dee2f86831f4dcfcbe3c8d79060098c8b73c10f
themeshaper <themeshaper@d957f892-c61d-0410-b221-f235e6eecf30>
2011-08-19 13:58:49 +0000

    Toolbox: * mimic the twentyeleven_posted_on() function * add hiding of single author links with is_multi_author()
    git-svn-id: https://wpcom-themes.svn.automattic.com/toolbox@7137 d957f892-c61d-0410-b221-f235e6eecf30

--- functions.php
+++ functions.php
@@ -124,6 +124,8 @@ add_action( 'init', 'toolbox_widgets_init' );
 if ( ! function_exists( 'toolbox_content_nav' ) ):
 /**
  * Display navigation to next/previous pages when applicable
+ *
+ * @since Toolbox 1.2
  */
 function toolbox_content_nav( $nav_id ) {
 	global $wp_query;
@@ -213,6 +215,42 @@ function toolbox_comment( $comment, $args, $depth ) {
 	endswitch;
 }
 endif; // ends check for toolbox_comment()
+
+if ( ! function_exists( 'toolbox_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ * Create your own toolbox_posted_on to override in a child theme
+ *
+ * @since Toolbox 1.2
+ */
+function toolbox_posted_on() {
+	printf( __( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'toolbox' ),
+		esc_url( get_permalink() ),
+		esc_attr( get_the_time() ),
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		sprintf( esc_attr__( 'View all posts by %s', 'toolbox' ), get_the_author() ),
+		esc_html( get_the_author() )
+	);
+}
+endif;
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @since Toolbox 1.2
+ */
+function toolbox_body_classes( $classes ) {
+
+	if ( ! is_multi_author() ) {
+		$classes[] = 'single-author';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'toolbox_body_classes' );
+
 
 /**
  * This theme was built with PHP, Semantic HTML, CSS, love, and a Toolbox.
